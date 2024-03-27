9a4ea01cefcde26e33ea3e7cbbe8a6b1b398a131
devJWL <jwlee4450@gmail.com>
2024-03-27 23:23:08 +0900

    🎉 init commit

---
+++ src/test/java/com/junwoo/ott/OttApplicationTests.java
@@ -0,0 +1,13 @@
+package com.junwoo.ott;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OttApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
