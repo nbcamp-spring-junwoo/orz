418e65d0eee09268c2bc01efccda9abf975004e4
devJWL <jwlee4450@gmail.com>
2024-03-29 11:49:34 +0900

    ✨ feat(auth): 회원가입을 위한 기본 코드 작성

---
+++ src/main/java/com/junwoo/ott/domain/auth/dto/reponse/AuthSignupRequestDto.java
@@ -0,0 +1,20 @@
+package com.junwoo.ott.domain.auth.dto.reponse;
+
+import com.junwoo.ott.domain.auth.dto.body.AuthSignupDto;
+import lombok.Getter;
+
+@Getter
+public class AuthSignupRequestDto {
+
+  private final String username;
+  private final String password;
+  private final String email;
+  private final String born;
+
+  public AuthSignupRequestDto(AuthSignupDto authSignupDto) {
+    this.username = authSignupDto.getUsername();
+    this.password = authSignupDto.getPassword();
+    this.email = authSignupDto.getEmail();
+    this.born = authSignupDto.getBorn();
+  }
+}
---
+++ src/main/java/com/junwoo/ott/domain/auth/service/AuthService.java
@@ -0,0 +1,14 @@
+package com.junwoo.ott.domain.auth.service;
+
+import com.junwoo.ott.domain.auth.dto.reponse.AuthSignupRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class AuthService {
+
+  public void signup(AuthSignupRequestDto authSignupRequestDto) {
+
+  }
+}
