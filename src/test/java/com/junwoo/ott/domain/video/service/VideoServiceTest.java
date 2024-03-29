53c5f84a54c6d69d7cfdfaae06ceacd65ac871f8
ksj58884 <ksj58884@gmail.com>
2024-03-29 16:13:03 +0900

    :art: feat(video): 테스트 공용 변수추가

--- src/test/java/com/junwoo/ott/domain/video/service/VideoServiceTest.java
+++ src/test/java/com/junwoo/ott/domain/video/service/VideoServiceTest.java
@@ -6,11 +6,10 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import com.junwoo.ott.domain.video.dto.request.VideoCreateRequestDto;
+import com.junwoo.ott.domain.video.common.VideoTestCommons;
 import com.junwoo.ott.domain.video.dto.response.VideoCreateResponseDto;
 import com.junwoo.ott.domain.video.entity.Video;
 import com.junwoo.ott.domain.video.repository.VideoJpaRepository;
-import com.junwoo.ott.global.customenum.RatingType;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -20,7 +19,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
-class VideoServiceTest {
+class VideoServiceTest implements VideoTestCommons {
 
   @Mock
   private VideoJpaRepository videoJpaRepository;
@@ -36,44 +35,38 @@ class VideoServiceTest {
     @DisplayName("생성 성공")
     void 비디오생성() {
       // given
-      VideoCreateRequestDto createRequestDto = new VideoCreateRequestDto("", "테스트설명1",
-          RatingType.EVERYONE);
       Video video = Video.builder()
-          .title(createRequestDto.getTitle())
-          .description(createRequestDto.getDescription())
-          .ratingType(createRequestDto.getRatingType())
+          .title(TEST_VIDEO_CREATE_REQUEST_DTO.getTitle())
+          .description(TEST_VIDEO_CREATE_REQUEST_DTO.getDescription())
+          .ratingType(TEST_VIDEO_CREATE_REQUEST_DTO.getRatingType())
           .build();
 
-      given(videoJpaRepository.save(any(Video.class))).willReturn(video);
+      given(videoJpaRepository.save(any())).willReturn(video);
 
       // when
-      VideoCreateResponseDto result = videoService.createVideo(createRequestDto);
+      VideoCreateResponseDto dto = videoService.createVideo(TEST_VIDEO_CREATE_REQUEST_DTO);
 
       // then
-      assertEquals(result.getTitle(), createRequestDto.getTitle());
-      assertEquals(result.getDescription(), createRequestDto.getDescription());
-      assertEquals(result.getRatingType(), createRequestDto.getRatingType());
-
+      assertEquals(TEST_TITLE, dto.getTitle());
+      assertEquals(TEST_DESCRIPTION, dto.getDescription());
+      assertEquals(TEST_RATING_TYPE, dto.getRatingType());
     }
+  }
 
-    @Nested
-    @DisplayName("비디오 생성 실패")
-    class CreateVideoFailure {
+  @Nested
+  @DisplayName("비디오 생성 실패")
+  class CreateVideoFailure {
 
-      @Test
-      @DisplayName("저장 시 예외 발생")
-      void 비디오생성실패() {
-        // given
-        VideoCreateRequestDto createRequestDto = new VideoCreateRequestDto("테스트제목1", "테스트설명1",
-            RatingType.RATE7);
-        given(videoJpaRepository.save(any(Video.class))).willThrow(new RuntimeException("생성 실패"));
+    @Test
+    @DisplayName("생성 실패")
+    void 비디오생성실패() {
+      // given
+      given(videoJpaRepository.save(any())).willThrow(new RuntimeException("생성 실패"));
 
-        // when & then
-        assertThrows(RuntimeException.class, () -> videoService.createVideo(createRequestDto),
-            "생성 실패");
-      }
+      // when & then
+      assertThrows(RuntimeException.class,
+          () -> videoService.createVideo(TEST_VIDEO_CREATE_REQUEST_DTO), "생성 실패");
     }
-
   }
 
 }
