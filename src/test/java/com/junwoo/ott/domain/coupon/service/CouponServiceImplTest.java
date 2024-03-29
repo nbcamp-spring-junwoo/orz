fed0b49db9ec1341f07ec7b488af0b59d1b2161a
gloz0315 <qudtn0315@naver.com>
2024-03-29 19:31:02 +0900

    ✅ test(coupon): 쿠폰 조회 테스트 성공 생성

--- src/test/java/com/junwoo/ott/domain/coupon/service/CouponServiceImplTest.java
+++ src/test/java/com/junwoo/ott/domain/coupon/service/CouponServiceImplTest.java
@@ -5,9 +5,15 @@ import static org.mockito.BDDMockito.given;
 
 import com.junwoo.ott.domain.coupon.dto.request.CouponCreateRequestDto;
 import com.junwoo.ott.domain.coupon.dto.response.CouponCreateResponseDto;
+import com.junwoo.ott.domain.coupon.dto.response.CouponIssuanceReadResponseDto;
+import com.junwoo.ott.domain.coupon.dto.response.CouponReadResponseDto;
 import com.junwoo.ott.domain.coupon.entity.Coupon;
+import com.junwoo.ott.domain.coupon.entity.CouponIssuance;
+import com.junwoo.ott.domain.coupon.repository.CouponIssuanceRepository;
 import com.junwoo.ott.domain.coupon.repository.CouponRepository;
 import com.junwoo.ott.domain.coupon.test.CouponTestValues;
+import com.junwoo.ott.domain.coupon.test.CouponUserTestValues;
+import java.util.List;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -16,12 +22,17 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 
 @ExtendWith(MockitoExtension.class)
-class CouponServiceImplTest implements CouponTestValues {
+class CouponServiceImplTest implements CouponTestValues, CouponUserTestValues {
 
   @Mock
   private CouponRepository couponRepository;
+
+  @Mock
+  private CouponIssuanceRepository couponIssuanceRepository;
 
   @InjectMocks
   private CouponService couponService;
@@ -44,6 +55,57 @@ class CouponServiceImplTest implements CouponTestValues {
       Assertions.assertEquals(result.getCouponType(), createRequestDto.getType());
       Assertions.assertEquals(result.getStartAt(), createRequestDto.getStartAt());
       Assertions.assertEquals(result.getEndAt(), createRequestDto.getEndAt());
+    }
+
+  }
+
+  @Nested
+  @DisplayName("쿠폰 테이블 조회")
+  class ReadCoupon {
+
+    @Test
+    @DisplayName("쿠폰 테이블 조회 성공")
+    void 쿠폰_테이블_조회_성공() {
+      // given
+      given(couponRepository.getCoupons(any())).willReturn(TEST_COUPONS);
+
+      // when
+      Page<CouponReadResponseDto> result = couponService.getCoupon(TEST_COUPON_READ_REQUEST_DTO);
+
+      // then
+      Assertions.assertEquals(result.getContent().size(), TEST_COUPON_SIZE);
+      Assertions.assertEquals(result.getContent().get(0).getCount(),
+          TEST_COUPON_CREATE_REQUEST_DTO.getCount());
+      Assertions.assertEquals(result.getContent().get(0).getCouponType(),
+          TEST_COUPON_CREATE_REQUEST_DTO.getType());
+      Assertions.assertEquals(result.getContent().get(0).getDiscount(),
+          TEST_COUPON_CREATE_REQUEST_DTO.getDiscount());
+    }
+
+  }
+
+  @Nested
+  @DisplayName("쿠폰 발급 테이블 조회")
+  class ReadCouponIssuance {
+
+    @Test
+    @DisplayName("쿠폰 발급 테이블 조회 성공")
+    void 쿠폰_발급테이블_조회_성공() {
+      // given
+      Page<CouponIssuance> couponIssuance = new PageImpl<>(
+          List.of(CouponIssuance.of(TEST_COUPON_V2, TEST_USER)));
+      given(couponIssuanceRepository.getCouponIssuance(any(), any())).willReturn(couponIssuance);
+
+      // when
+      Page<CouponIssuanceReadResponseDto> result = couponService.getCoupons(
+          TEST_COUPON_READ_REQUEST_DTO);
+
+      // then
+      Assertions.assertEquals(result.getContent().get(0).getCouponId(),
+          TEST_COUPON_V2.getCouponId());
+      Assertions.assertEquals(result.getContent().get(0).getCouponType(),
+          TEST_COUPON_V1.getCouponType());
+      Assertions.assertEquals(result.getContent().get(0).getMembershipType(), TEST_MEMBERSHIP);
     }
 
   }
---
+++ src/test/java/com/junwoo/ott/domain/coupon/test/CouponUserTestValues.java
@@ -0,0 +1,29 @@
+package com.junwoo.ott.domain.coupon.test;
+
+import com.junwoo.ott.domain.user.entity.User;
+import com.junwoo.ott.global.customenum.AuthorityType;
+import com.junwoo.ott.global.customenum.MembershipType;
+import java.time.LocalDate;
+
+public interface CouponUserTestValues {
+
+  Long TEST_USER_ID = 1L;
+  String TEST_USER_NAME = "TEST";
+  String TEST_USER_PASSWORD = "TEST_PASSWORD_123";
+  String TEST_EMAIL = "Test123@naver.com";
+  LocalDate TEST_BORN = LocalDate.of(1997, 3, 15);
+  AuthorityType TEST_ADMIN_TYPE = AuthorityType.ADMIN;
+  AuthorityType TEST_USER_TYPE = AuthorityType.USER;
+  MembershipType TEST_MEMBERSHIP = MembershipType.GOLD;
+
+  User TEST_USER = User.builder()
+      .userId(TEST_USER_ID)
+      .username(TEST_USER_NAME)
+      .password(TEST_USER_PASSWORD)
+      .email(TEST_EMAIL)
+      .born(TEST_BORN)
+      .authorityType(TEST_ADMIN_TYPE)
+      .membershipType(TEST_MEMBERSHIP)
+      .build();
+
+}
