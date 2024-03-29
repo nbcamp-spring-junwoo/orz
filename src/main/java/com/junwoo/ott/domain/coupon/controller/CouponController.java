95e1c6186014f0949266faff4ec2eb8327cd1914
gloz0315 <qudtn0315@naver.com>
2024-03-29 19:44:06 +0900

    🎨 style(coupon): 어노테이션 순서 변경

--- src/main/java/com/junwoo/ott/domain/coupon/controller/CouponController.java
+++ src/main/java/com/junwoo/ott/domain/coupon/controller/CouponController.java
@@ -18,8 +18,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/api/v1")
 @RequiredArgsConstructor
+@RequestMapping("/api/v1")
 @RestController
 public class CouponController {
 
