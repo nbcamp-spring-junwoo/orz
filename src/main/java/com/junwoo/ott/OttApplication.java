1e440d474e52a58034f89d0821c27a78c1776ee1
devJWL <jwlee4450@gmail.com>
2024-03-29 10:02:35 +0900

    🐛 fix(auth): security관련 버그 수정 애노테이션 추가

--- src/main/java/com/junwoo/ott/OttApplication.java
+++ src/main/java/com/junwoo/ott/OttApplication.java
@@ -1,9 +1,12 @@
 package com.junwoo.ott;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class,
+    ManagementWebSecurityAutoConfiguration.class})
 public class OttApplication {
 
   public static void main(String[] args) {
