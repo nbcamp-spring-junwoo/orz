3657091fb90d70b054e8bd8f1178c225e68d39a3
gloz0315 <qudtn0315@naver.com>
2024-03-29 10:48:34 +0900

    🎨 style(coupon): enumType 별 @Enumerated 어노테이션 붙임

--- src/main/java/com/junwoo/ott/domain/coupon/entity/Coupon.java
+++ src/main/java/com/junwoo/ott/domain/coupon/entity/Coupon.java
@@ -1,10 +1,13 @@
 package com.junwoo.ott.domain.coupon.entity;
 
+import com.junwoo.ott.domain.coupon.dto.request.CouponCreateRequestDto;
 import com.junwoo.ott.global.common.entity.Timestamped;
 import com.junwoo.ott.global.customenum.CouponType;
 import com.junwoo.ott.global.customenum.MembershipType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -32,7 +35,9 @@ public class Coupon extends Timestamped {
   private Long couponId;
   private String description;
   @Column(nullable = false)
+  @Enumerated(EnumType.STRING)
   private CouponType couponType;
+  @Enumerated(EnumType.STRING)
   private MembershipType membershipType;
   @Column(nullable = false)
   private Integer count;
@@ -43,5 +48,18 @@ public class Coupon extends Timestamped {
   @Column(nullable = false)
   private LocalDate endAt;
   private LocalDateTime deletedAt;
+
+  public static Coupon of(CouponCreateRequestDto dto) {
+
+    return Coupon.builder()
+        .description(dto.getDescription())
+        .couponType(CouponType.valueOf(dto.getType()))
+        .membershipType(MembershipType.valueOf(dto.getMembershipType()))
+        .count(dto.getCount())
+        .discount(dto.getDiscount())
+        .startAt(dto.getStartAt())
+        .endAt(dto.getEndAt())
+        .build();
+  }
 
 }
