9543892ca2b131afff2e8ad1f5670fa0a0dc0412
ksj58884 <ksj58884@gmail.com>
2024-03-29 15:53:51 +0900

    :sparkles: feat(video): createdto 생성

---
+++ src/main/java/com/junwoo/ott/domain/video/dto/request/VideoCreateRequestDto.java
@@ -0,0 +1,23 @@
+package com.junwoo.ott.domain.video.dto.request;
+
+import com.junwoo.ott.domain.video.dto.body.VideoCreateDto;
+import com.junwoo.ott.global.customenum.RatingType;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class VideoCreateRequestDto {
+    private String title;
+    private String description;
+    private RatingType ratingType;
+
+    public VideoCreateRequestDto(VideoCreateDto dto) {
+        this.title = dto.getTitle();
+        this.description = dto.getDescription();
+        this.ratingType = dto.getRatingType();
+    }
+
+}
---
+++ src/main/java/com/junwoo/ott/domain/video/dto/response/VideoCreateResponseDto.java
@@ -0,0 +1,30 @@
+package com.junwoo.ott.domain.video.dto.response;
+
+import com.junwoo.ott.domain.video.entity.Video;
+import com.junwoo.ott.global.customenum.RatingType;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class VideoCreateResponseDto {
+    private Long videoId;
+    private String title;
+    private String description;
+    private RatingType ratingType;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public VideoCreateResponseDto(Video video) {
+        this.videoId = video.getVideoId();
+        this.title = video.getTitle();
+        this.description = video.getDescription();
+        this.ratingType = video.getRatingType();
+        this.createdAt = video.getCreatedAt();
+        this.updatedAt = video.getUpdatedAt();
+    }
+
+}
