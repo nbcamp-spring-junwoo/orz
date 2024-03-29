11a98183355bdbfbc146e521d6ea985eeb55f4f7
gloz0315 <qudtn0315@naver.com>
2024-03-29 20:45:28 +0900

    🎨 style(coupon): 컨트롤러 파라미터 변경
    - 페이지와 사이즈 받은 것은 Pageable 객체로 변경

--- src/main/java/com/junwoo/ott/domain/coupon/controller/CouponController.java
+++ src/main/java/com/junwoo/ott/domain/coupon/controller/CouponController.java
@@ -9,13 +9,13 @@ import com.junwoo.ott.domain.coupon.service.CouponService;
 import com.junwoo.ott.global.common.dto.ResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
@@ -26,11 +26,9 @@ public class CouponController {
   private final CouponService couponService;
 
   @GetMapping("/coupons")
-  public ResponseDto<Page<CouponReadResponseDto>> getCoupons(
-      @RequestParam("page") int page,
-      @RequestParam("size") int size
-  ) {
-    CouponReadRequestDto dto = new CouponReadRequestDto(page, size);
+  public ResponseDto<Page<CouponReadResponseDto>> getCoupons(Pageable pageable) {
+    CouponReadRequestDto dto = new CouponReadRequestDto(pageable.getPageNumber(),
+        pageable.getPageSize());
     Page<CouponReadResponseDto> result = couponService.getCoupon(dto);
 
     return ResponseDto.ok(result);
@@ -39,10 +37,10 @@ public class CouponController {
   @GetMapping("users/{userId}/coupons")
   public ResponseDto<Page<CouponIssuanceReadResponseDto>> getCoupons(
       @PathVariable("userId") Long userId,
-      @RequestParam("page") int page,
-      @RequestParam("size") int size
+      Pageable pageable
   ) {
-    CouponReadRequestDto dto = new CouponReadRequestDto(userId, page, size);
+    CouponReadRequestDto dto = new CouponReadRequestDto(userId, pageable.getPageNumber(),
+        pageable.getPageSize());
     Page<CouponIssuanceReadResponseDto> result = couponService.getCoupons(dto);
 
     return ResponseDto.ok(result);
