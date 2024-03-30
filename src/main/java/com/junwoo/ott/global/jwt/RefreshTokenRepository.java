6f1d079706f93c9d8720bcc0067adf1bbeb6a9eb
devJWL <jwlee4450@gmail.com>
2024-03-30 20:08:04 +0900

    ✨ feat(security): jwt 토큰 관련 클래스 구현

---
+++ src/main/java/com/junwoo/ott/global/jwt/RefreshTokenRepository.java
@@ -0,0 +1,33 @@
+package com.junwoo.ott.global.jwt;
+
+import jakarta.annotation.Resource;
+import java.util.concurrent.TimeUnit;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class RefreshTokenRepository {
+
+  private static final String USER_TOKEN_PREFIX = "user_token_";
+  private final RedisTemplate<String, String> redisTemplate;
+  @Resource(name = "redisTemplate")
+  private ValueOperations<String, String> valueOperations;
+
+  public void delete(String username) {
+    redisTemplate.delete(
+        USER_TOKEN_PREFIX + username);
+  }
+
+  public void save(String username, String refreshToken) {
+    valueOperations.set(USER_TOKEN_PREFIX + username, refreshToken);
+    redisTemplate.expire(USER_TOKEN_PREFIX + username, 7L, TimeUnit.DAYS);
+  }
+
+  public boolean existsByKey(String key) {
+    return Boolean.TRUE.equals(valueOperations.getOperations().hasKey(USER_TOKEN_PREFIX + key));
+  }
+
+}
