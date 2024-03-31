7a567078e303f2b2308e7b627e89479cf013ca0d
devJWL <jwlee4450@gmail.com>
2024-03-31 22:06:08 +0900

    ✅ fix(user): 회원정보 수정 테스트 코드 작성

--- src/test/java/com/junwoo/ott/domain/user/UserTestValues.java
+++ src/test/java/com/junwoo/ott/domain/user/UserTestValues.java
@@ -2,28 +2,43 @@ package com.junwoo.ott.domain.user;
 
 import com.junwoo.ott.domain.auth.dto.body.AuthSignupDto;
 import com.junwoo.ott.domain.auth.dto.request.AuthSignupRequestDto;
+import com.junwoo.ott.domain.user.dto.body.UserPutDto;
+import com.junwoo.ott.domain.user.dto.request.UserPutRequestDto;
 import com.junwoo.ott.domain.user.entity.User;
 import java.time.LocalDate;
 
 public interface UserTestValues {
 
   Long TEST_USER_ID = 1L;
+  Long TEST_OTHER_USER_ID = 2L;
   String TEST_USERNAME = "test1";
   String TEST_PASSWORD = "password";
-  String ENCRYPT_PASSWORD = "encryptPassword";
+  String TEST_NEW_PASSWORD = "newpassword";
+  String TEST_ENCRYPT_PASSWORD = "encryptPassword";
+  String TEST_NEW_ENCRYPT_PASSWORD = "newEncryptPassword";
   String TEST_EMAIL = "test1@email.com";
+  String TEST_NEW_EMAIL = "newtest1@email.com";
   String TEST_BORN = "2001-01-05";
+  String TEST_NEW_BORN = "2001-01-06";
   LocalDate TEST_DATE_BORN = LocalDate.parse(TEST_BORN);
-
-  String TEST_USERNAME_ALREADY_EXIST_EXCEPTION_MESSAGE = "이미 존재하는 username입니다.";
+  LocalDate TEST_NEW_DATE_BORN = LocalDate.parse(TEST_NEW_BORN);
   AuthSignupRequestDto TEST_AUTH_SIGNUP_REQUEST_DTO = new AuthSignupRequestDto(
       new AuthSignupDto(TEST_USERNAME, TEST_PASSWORD, TEST_EMAIL, TEST_BORN)
   );
 
+  UserPutRequestDto TEST_USER_PUT_REQUEST_DTO = new UserPutRequestDto(
+      TEST_USER_ID, TEST_USER_ID,
+      new UserPutDto(TEST_PASSWORD, TEST_NEW_PASSWORD, TEST_NEW_EMAIL, TEST_NEW_BORN)
+  );
+
+  UserPutRequestDto TEST_USER_PUT_REQUEST_DTO_MISMATCH = new UserPutRequestDto(
+      TEST_USER_ID, TEST_OTHER_USER_ID,
+      new UserPutDto(TEST_PASSWORD, TEST_NEW_PASSWORD, TEST_NEW_EMAIL, TEST_NEW_BORN));
+
   User TEST_USER = User.builder()
       .userId(TEST_USER_ID)
       .username(TEST_USERNAME)
-      .password(ENCRYPT_PASSWORD)
+      .password(TEST_ENCRYPT_PASSWORD)
       .email(TEST_EMAIL)
       .born(TEST_DATE_BORN)
       .build();
