fbff5a6545145f3a37e856617f3a990da610e29c
tktcod <hs060100@gmail.com>
2024-04-01 02:24:15 +0900

    :white_check_mark: test(chart): 탑 비디오 요청 테스트 생성

--- src/test/java/com/junwoo/ott/domain/chart/service/ChartHistoryServiceTest.java
+++ src/test/java/com/junwoo/ott/domain/chart/service/ChartHistoryServiceTest.java
@@ -1,10 +1,14 @@
 package com.junwoo.ott.domain.chart.service;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
 
 import com.junwoo.ott.domain.chart.test.ChartTestValues;
+import java.util.Set;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -12,6 +16,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.redis.core.DefaultTypedTuple;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ZSetOperations;
 
@@ -43,6 +48,25 @@ public class ChartHistoryServiceTest implements ChartTestValues {
   }
 
   @Test
+  @DisplayName("탑 비디오 요청 테스트")
+  void getTopVideosTest() {
+    // given
+    Set<ZSetOperations.TypedTuple<String>> typedTuples = Set.of(
+        new DefaultTypedTuple<>(String.valueOf(TEST_VIDEO_ID_V1), TEST_VIDEO_POINT_V1),
+        new DefaultTypedTuple<>(String.valueOf(TEST_VIDEO_ID_V2), TEST_VIDEO_POINT_V2),
+        new DefaultTypedTuple<>(String.valueOf(TEST_VIDEO_ID_V3), TEST_VIDEO_POINT_V3)
+    );
+    given(zSetOps.reverseRangeWithScores(anyString(), anyLong(), anyLong())).willReturn(
+        typedTuples);
+
+    // when
+    var topVideos = chartHistoryService.getTopVideos();
+
+    // then
+    assertThat(topVideos).hasSize(3);
+  }
+
+  @Test
   @DisplayName("VideoPoint 삭제 테스트")
   void deleteVideoPointTest() {
     // when
@@ -51,4 +75,5 @@ public class ChartHistoryServiceTest implements ChartTestValues {
     // then
     verify(redisTemplate).delete(ChartHistoryService.VIDEO_POINT);
   }
+
 }
