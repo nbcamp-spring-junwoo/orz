c9414cf2e3ca2b8af2e8c179db9ab060de7379cf
tktcod <hs060100@gmail.com>
2024-04-01 00:38:11 +0900

    :sparkles: feat(chart) 차트 기록 추가 및 조회, 삭제 기능 추가

---
+++ src/main/java/com/junwoo/ott/domain/chart/service/ChartHistoryService.java
@@ -0,0 +1,48 @@
+package com.junwoo.ott.domain.chart.service;
+
+import com.junwoo.ott.domain.chart.dto.response.VideoPointResponseDto;
+import jakarta.annotation.PostConstruct;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ZSetOperations;
+import org.springframework.data.redis.core.ZSetOperations.TypedTuple;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class ChartHistoryService {
+
+  private static final String VIDEO_POINT = "videoPoint";
+  private static final Integer TOP_N = 10;
+
+  private final RedisTemplate<String, String> redisTemplate;
+  private ZSetOperations<String, String> zSetOps;
+
+  @PostConstruct
+  private void init() {
+    zSetOps = redisTemplate.opsForZSet();
+  }
+
+  public void addVideoPoint(Long videoId, double point) {
+    zSetOps.add(VIDEO_POINT, String.valueOf(videoId), point);
+  }
+
+  public List<VideoPointResponseDto> getTopVideos() {
+    Set<TypedTuple<String>> topVideosWithScores = Optional.ofNullable(
+            zSetOps.reverseRangeWithScores(VIDEO_POINT, 0, TOP_N - 1))
+        .orElse(Collections.emptySet());
+
+    return topVideosWithScores.stream()
+        .map(tuple -> new VideoPointResponseDto(Long.parseLong(tuple.getValue()), tuple.getScore()))
+        .toList();
+  }
+
+  public void deleteVideoPoint() {
+    redisTemplate.delete(VIDEO_POINT);
+  }
+
+}
