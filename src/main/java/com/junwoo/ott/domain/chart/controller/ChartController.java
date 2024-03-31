0a9924e809a0ecebeda31b5a49baf18dff9d2876
tktcod <hs060100@gmail.com>
2024-04-01 00:41:06 +0900

    :sparkles: feat(chart) 차트 컨트롤러 추가

---
+++ src/main/java/com/junwoo/ott/domain/chart/controller/ChartController.java
@@ -0,0 +1,24 @@
+package com.junwoo.ott.domain.chart.controller;
+
+import com.junwoo.ott.domain.chart.dto.response.VideoPointResponseDto;
+import com.junwoo.ott.domain.chart.service.ChartService;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RequestMapping("api/v1")
+@RestController
+public class ChartController {
+
+
+  private final ChartService chartService;
+
+  @GetMapping("/chart")
+  public List<VideoPointResponseDto> getChart() {
+    return chartService.getChart();
+  }
+
+}
