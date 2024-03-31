12cdf82d2b9ed92a1710d53e9bef6b488d334e8c
devJWL <jwlee4450@gmail.com>
2024-03-31 22:05:40 +0900

    ✨ fix(user): 회원정보 수정 기능구현

---
+++ src/main/java/com/junwoo/ott/global/exception/custom/NotExistAuthorityTypeException.java
@@ -0,0 +1,9 @@
+package com.junwoo.ott.global.exception.custom;
+
+public class NotExistAuthorityTypeException extends RuntimeException {
+
+  public NotExistAuthorityTypeException(String message) {
+    super(message);
+  }
+
+}
---
+++ src/main/java/com/junwoo/ott/global/exception/custom/NotExistMembershipTypeException.java
@@ -0,0 +1,9 @@
+package com.junwoo.ott.global.exception.custom;
+
+public class NotExistMembershipTypeException extends RuntimeException {
+
+  public NotExistMembershipTypeException(String message) {
+    super(message);
+  }
+
+}
