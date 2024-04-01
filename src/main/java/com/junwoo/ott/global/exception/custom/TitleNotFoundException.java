09e7f657aa5db2410fb48e8b3cf078682640a7a4
ksj58884 <ksj58884@gmail.com>
2024-04-01 10:40:20 +0900

    :art: feat(video): 피드백 반영 제목조회 예외처리 커스텀화

---
+++ src/main/java/com/junwoo/ott/global/exception/custom/TitleNotFoundException.java
@@ -0,0 +1,9 @@
+package com.junwoo.ott.global.exception.custom;
+
+public class TitleNotFoundException extends RuntimeException {
+
+  public TitleNotFoundException(String message) {
+    super(message);
+  }
+
+}
