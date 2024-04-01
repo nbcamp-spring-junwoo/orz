d5f3448744053f3b458fb49437a366b867b1cd3a
gloz0315 <qudtn0315@naver.com>
2024-04-01 14:44:07 +0900

    ♻️ refactor(coupon): CouponIssuance에 대한 JPQL -> QueryDsl로 코드 변경

--- src/main/java/com/junwoo/ott/domain/coupon/repository/CouponIssuanceRepository.java
+++ src/main/java/com/junwoo/ott/domain/coupon/repository/CouponIssuanceRepository.java
@@ -1,14 +1,10 @@
 package com.junwoo.ott.domain.coupon.repository;
 
 import com.junwoo.ott.domain.coupon.entity.CouponIssuance;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
+import com.junwoo.ott.domain.coupon.repository.queryDsl.CouponIssuanceQueryDsl;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
-public interface CouponIssuanceRepository extends JpaRepository<CouponIssuance, Long> {
-
-  @Query("select c from CouponIssuance c where c.usedAt is null and c.user.userId = :userId order by c.issuedAt desc")
-  Page<CouponIssuance> getCouponIssuance(final Long userId, final Pageable pageable);
+public interface CouponIssuanceRepository extends JpaRepository<CouponIssuance, Long>,
+    CouponIssuanceQueryDsl {
 
 }
