fbb0c5a8348aa9c8b50bde918a81b694dba8e533
ksj58884 <ksj58884@gmail.com>
2024-04-01 15:59:28 +0900

    :twisted_rightwards_arrows: feat(video): 머지 충돌 해결

--- src/main/java/com/junwoo/ott/domain/video/controller/VideoController.java
+++ src/main/java/com/junwoo/ott/domain/video/controller/VideoController.java
@@ -2,12 +2,20 @@ package com.junwoo.ott.domain.video.controller;
 
 import com.junwoo.ott.domain.video.dto.body.VideoCreateDto;
 import com.junwoo.ott.domain.video.dto.request.VideoCreateRequestDto;
+import com.junwoo.ott.domain.video.dto.request.VideoReadRequestDto;
+import com.junwoo.ott.domain.video.dto.response.VideoReadResponseDto;
 import com.junwoo.ott.domain.video.service.VideoService;
+import com.junwoo.ott.global.common.dto.ResponseDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/api/v1/videos")
@@ -18,9 +26,32 @@ public class VideoController {
   private final VideoService videoService;
 
   @PostMapping
-  public void postVideo(@Validated @RequestBody VideoCreateDto dto) {
+  public void postVideo(
+      @Validated @RequestBody VideoCreateDto dto
+  ) {
     VideoCreateRequestDto createDto = new VideoCreateRequestDto(dto);
     videoService.createVideo(createDto);
+  }
+
+  @GetMapping
+  public ResponseDto<Page<VideoReadResponseDto>> getVideos(Pageable pageable) {
+    VideoReadRequestDto dto = new VideoReadRequestDto(pageable);
+    Page<VideoReadResponseDto> videosPage = videoService.getVideos(dto);
+
+    return ResponseDto.ok(videosPage);
+  }
+
+  @GetMapping("{videoId}")
+  public ResponseDto<VideoReadResponseDto> getVideo(@PathVariable("videoId") Long videoId) {
+    VideoReadResponseDto videoReadResponseDto = videoService.getVideo(videoId);
+    return ResponseDto.ok(videoReadResponseDto);
+  }
+
+  @GetMapping("/search")
+  public ResponseDto<Page<VideoReadResponseDto>> searchVideos(@RequestParam(required = false) String title, Pageable pageable) {
+    VideoReadRequestDto dto = new VideoReadRequestDto(title, pageable);
+    Page<VideoReadResponseDto> videosPage = videoService.getVideosByTitle(dto);
+    return ResponseDto.ok(videosPage);
   }
 
 }
---
+++ src/main/java/com/junwoo/ott/domain/video/dto/request/VideoReadRequestDto.java
@@ -0,0 +1,23 @@
+package com.junwoo.ott.domain.video.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.data.domain.Pageable;
+
+@Getter
+@AllArgsConstructor
+public class VideoReadRequestDto {
+    private Long videoId;
+    private String title;
+    private Pageable pageable;
+
+    public VideoReadRequestDto(Pageable pageable) {
+        this.pageable = pageable;
+    }
+
+    public VideoReadRequestDto(String title, Pageable pageable) {
+        this.title = title;
+        this.pageable = pageable;
+    }
+
+}
