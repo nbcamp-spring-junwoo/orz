269c1cda586961bbbcfab2d3df97a93de6c3dc84
tktcod <hs060100@gmail.com>
2024-04-01 12:02:40 +0900

    :white_check_mark: test(chart) 차트 조회 테스트 생성

--- src/test/java/com/junwoo/ott/domain/chart/service/ChartServiceTest.java
+++ src/test/java/com/junwoo/ott/domain/chart/service/ChartServiceTest.java
@@ -1,10 +1,12 @@
 package com.junwoo.ott.domain.chart.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.argThat;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
 
+import com.junwoo.ott.domain.chart.dto.response.ChartResponseDto;
 import com.junwoo.ott.domain.chart.dto.response.VideoPointResponseDto;
 import com.junwoo.ott.domain.chart.entity.Chart;
 import com.junwoo.ott.domain.chart.repository.ChartJpaRepository;
@@ -31,6 +33,24 @@ public class ChartServiceTest implements ChartTestValues {
   private ChartHistoryService chartHistoryService;
   @Mock
   private ChartJpaRepository chartJpaRepository;
+
+  @Test
+  @DisplayName("차트 조회 테스트")
+  void 차트_조회() {
+    // given
+    List<ChartResponseDto> chartResponse = List.of(
+        new ChartResponseDto(TEST_VIDEO_ID_V1, TEST_VIDEO_TITLE_V1),
+        new ChartResponseDto(TEST_VIDEO_ID_V2, TEST_VIDEO_TITLE_V2),
+        new ChartResponseDto(TEST_VIDEO_ID_V3, TEST_VIDEO_TITLE_V3)
+    );
+    given(chartJpaRepository.getAllChartsWithVideoInfo()).willReturn(chartResponse);
+
+    // when
+    List<ChartResponseDto> result = chartService.getChart();
+
+    // then
+    assertEquals(result, chartResponse);
+  }
 
   @Test
   @DisplayName("차트 업데이트 테스트")
