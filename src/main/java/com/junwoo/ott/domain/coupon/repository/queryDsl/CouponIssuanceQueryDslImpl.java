7e73ddd9d629a5e02e13741f019b55ee50c76605
gloz0315 <qudtn0315@naver.com>
2024-04-01 14:44:07 +0900

    ♻️ refactor(coupon): CouponIssuance에 대한 JPQL -> QueryDsl로 코드 변경

---
+++ src/main/java/com/junwoo/ott/domain/coupon/repository/queryDsl/CouponIssuanceQueryDslImpl.java
@@ -0,0 +1,46 @@
+package com.junwoo.ott.domain.coupon.repository.queryDsl;
+
+import static com.junwoo.ott.domain.coupon.entity.QCouponIssuance.couponIssuance;
+
+import com.junwoo.ott.domain.coupon.entity.CouponIssuance;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQuery;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.support.PageableExecutionUtils;
+import org.springframework.stereotype.Repository;
+
+@RequiredArgsConstructor
+@Repository
+public class CouponIssuanceQueryDslImpl implements CouponIssuanceQueryDsl {
+
+  private final JPAQueryFactory jpaQueryFactory;
+
+  @Override
+  public Page<CouponIssuance> getCouponIssuance(final Long userId, final Pageable pageable) {
+
+    List<CouponIssuance> result = jpaQueryFactory.select(couponIssuance)
+        .from(couponIssuance)
+        .where(notUsedAndUserIdEq(userId))
+        .orderBy(couponIssuance.issuedAt.desc())
+        .fetch();
+
+    JPAQuery<Long> count = jpaQueryFactory
+        .select(couponIssuance.count())
+        .from(couponIssuance);
+
+    return PageableExecutionUtils.getPage(result, pageable, count::fetchOne);
+  }
+
+  private BooleanExpression userIdEq(final Long userId) {
+    return userId != null ? couponIssuance.user.userId.eq(userId) : null;
+  }
+
+  private BooleanExpression notUsedAndUserIdEq(final Long userId) {
+    return couponIssuance.usedAt.isNull().and(userIdEq(userId));
+  }
+
+}
