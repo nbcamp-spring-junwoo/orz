9f8beaf14d5cc607768eede76b6e343f5ba1c873
ksj58884 <ksj58884@gmail.com>
2024-04-01 16:05:56 +0900

    :twisted_rightwards_arrows: feat(video): 머지 충돌 해결

--- src/main/java/com/junwoo/ott/domain/video/repository/VideoJpaRepository.java
+++ src/main/java/com/junwoo/ott/domain/video/repository/VideoJpaRepository.java
@@ -1,11 +1,8 @@
 package com.junwoo.ott.domain.video.repository;
 
 import com.junwoo.ott.domain.video.entity.Video;
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface VideoJpaRepository extends JpaRepository<Video, Long> {
-
-  List<Video> getByVideoIdIn(List<Long> videoIds);
+public interface VideoJpaRepository extends JpaRepository<Video, Long>, VideoCustomRepository {
 
 }
