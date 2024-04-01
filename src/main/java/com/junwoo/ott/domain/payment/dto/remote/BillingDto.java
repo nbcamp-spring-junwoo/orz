fdead1ed5269e43865248139e51f426d8f94b917
ruh0n <wns8256@naver.com>
2024-04-01 19:50:33 +0900

    ✨ feat(payment): 정기결제 결과를 받는 dto 추가

---
+++ src/main/java/com/junwoo/ott/domain/payment/dto/remote/BillingDto.java
@@ -0,0 +1,24 @@
+package com.junwoo.ott.domain.payment.dto.remote;
+
+import com.junwoo.ott.domain.payment.dto.remote.payment.CardDto;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class BillingDto {
+
+  // 상점아이디(MID)입니다. 토스페이먼츠에서 발급합니다. 최대 길이는 14자입니다.
+  private String mId;
+  // 구매자 ID입니다. 빌링키와 연결됩니다. 다른 사용자가 이 값을 알게 되면 악의적으로 사용될 수 있습니다.
+  private String customerKey;
+  // 결제수단이 인증된 시점의 날짜와 시간 정보입니다.
+  private String authenticatedAt;
+  // 결제수단입니다. 현재 자동결제는 카드만 지원하기 때문에 카드로 값이 고정되어 돌아옵니다.
+  private String method;
+  // 자동결제에서 카드 정보 대신 사용되는 값입니다. customerKey와 연결됩니다. 최대 길이는 200자입니다.
+  private String billingKey;
+  // 발급된 빌링키와 연결된 카드 정보입니다.
+  private CardDto card;
+
+}
