692dbb39161d0797003b7a3d3c14b2d88fd3be48
ruh0n <wns8256@naver.com>
2024-04-02 23:13:04 +0900

    ✅ test(membership): 멤버쉽 테스트 추가

---
+++ src/test/java/com/junwoo/ott/domain/membership/test/MembershipServiceTestValues.java
@@ -0,0 +1,13 @@
+package com.junwoo.ott.domain.membership.test;
+
+import com.junwoo.ott.domain.membership.entity.Membership;
+import com.junwoo.ott.global.customenum.MembershipType;
+
+public interface MembershipServiceTestValues {
+
+  Membership TEST_MEMBERSHIP = Membership.builder()
+      .membershipId(1L)
+      .membershipType(MembershipType.ROLE_NORMAL)
+      .build();
+
+}
