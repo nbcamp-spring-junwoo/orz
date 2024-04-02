540a2fd26afdfc0e78b5c3212eec441adac266f8
ksj58884 <ksj58884@gmail.com>
2024-04-02 19:14:27 +0900

    :sparkles: test(episode): 조회 기능 추가

--- src/main/java/com/junwoo/ott/domain/episode/controller/EpisodeController.java
+++ src/main/java/com/junwoo/ott/domain/episode/controller/EpisodeController.java
@@ -2,9 +2,14 @@ package com.junwoo.ott.domain.episode.controller;
 
 import com.junwoo.ott.domain.episode.dto.body.EpisodeCreateDto;
 import com.junwoo.ott.domain.episode.dto.request.EpisodeCreateRequestDto;
+import com.junwoo.ott.domain.episode.dto.response.EpisodeReadResponseDto;
 import com.junwoo.ott.domain.episode.service.EpisodeService;
+import com.junwoo.ott.global.common.dto.ResponseDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,9 +24,30 @@ public class EpisodeController {
     private final EpisodeService episodeService;
 
     @PostMapping
-    public void postEpisode(@PathVariable Long videoId, @Validated @RequestBody EpisodeCreateDto dto) {
+    public void postEpisode(
+        @PathVariable Long videoId,
+        @Validated @RequestBody EpisodeCreateDto dto
+    ) {
         EpisodeCreateRequestDto createDto = new EpisodeCreateRequestDto(dto.getTitle(), dto.getReleasedAt());
         episodeService.createEpisode(videoId, createDto);
+    }
+
+    @GetMapping
+    public ResponseDto<Page<EpisodeReadResponseDto>> getEpisodes(
+        @PathVariable Long videoId,
+        Pageable pageable
+    ) {
+        Page<EpisodeReadResponseDto> episodesPage = episodeService.getEpisodesByVideo(videoId, pageable);
+        return ResponseDto.ok(episodesPage);
+    }
+
+    @GetMapping("/{episodeId}")
+    public ResponseDto<EpisodeReadResponseDto> getEpisode(
+        @PathVariable Long videoId,
+        @PathVariable Long episodeId
+    ) {
+        EpisodeReadResponseDto episodeReadResponseDto = episodeService.getEpisodeByVideo(videoId, episodeId);
+        return ResponseDto.ok(episodeReadResponseDto);
     }
 
 }
