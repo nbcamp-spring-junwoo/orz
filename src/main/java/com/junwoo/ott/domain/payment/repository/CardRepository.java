b38ef3dbbe5f6b6cf1d543898ee2f7fc41cba577
ruh0n <wns8256@naver.com>
2024-04-02 16:03:12 +0900

    🩹 fix(payment): 매개변수에 final 속성 추가

--- src/main/java/com/junwoo/ott/domain/payment/repository/CardRepository.java
+++ src/main/java/com/junwoo/ott/domain/payment/repository/CardRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CardRepository extends JpaRepository<Card, Long> {
 
-  boolean existsByCardNumber(String cardNumber);
+  boolean existsByCardNumber(final String cardNumber);
 
 }
