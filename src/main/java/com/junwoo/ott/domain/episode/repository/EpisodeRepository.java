35f5c61ad5496b8759150b687c5c40f26ebd6374
ksj58884 <ksj58884@gmail.com>
2024-04-02 19:15:05 +0900

    :sparkles: feat(episode): 커스텀 리포지토리 추가

--- src/main/java/com/junwoo/ott/domain/episode/repository/EpisodeRepository.java
+++ src/main/java/com/junwoo/ott/domain/episode/repository/EpisodeRepository.java
@@ -3,6 +3,6 @@ package com.junwoo.ott.domain.episode.repository;
 import com.junwoo.ott.domain.episode.entity.Episode;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EpisodeRepository extends JpaRepository<Episode, Long> {
+public interface EpisodeRepository extends JpaRepository<Episode, Long>, EpisodeCustomRepository {
 
 }
