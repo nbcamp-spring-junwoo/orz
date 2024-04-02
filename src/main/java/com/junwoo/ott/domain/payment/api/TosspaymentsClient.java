5e98403332315119472871080a63d67bf53390d6
ruh0n <wns8256@naver.com>
2024-04-02 12:02:00 +0900

    🩹 fix(payment): 테스트 환경에서 발생하는 오류 수정

--- src/main/java/com/junwoo/ott/domain/payment/api/TosspaymentsClient.java
+++ src/main/java/com/junwoo/ott/domain/payment/api/TosspaymentsClient.java
@@ -1,7 +1,8 @@
 package com.junwoo.ott.domain.payment.api;
 
+import com.junwoo.ott.domain.payment.api.configuration.TosspaymentsFeignConfiguration;
 import com.junwoo.ott.domain.payment.dto.body.PaymentConfirmDto;
-import com.junwoo.ott.domain.payment.dto.body.PaymentDto;
+import com.junwoo.ott.domain.payment.dto.remote.PaymentDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 
