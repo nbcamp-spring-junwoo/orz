1edb056c57054aca587dea52c0fc80348f12e1d9
tktcod <hs060100@gmail.com>
2024-04-03 12:23:17 +0900

    :sparkles: feat(chart): chartUpdate job 생성

---
+++ src/main/java/com/junwoo/ott/global/batch/ChartUpdateJob.java
@@ -0,0 +1,42 @@
+package com.junwoo.ott.global.batch;
+
+import com.junwoo.ott.domain.chart.service.ChartService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.support.DefaultBatchConfiguration;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j(topic = "ChartUpdateJob")
+@Configuration
+@RequiredArgsConstructor
+public class ChartUpdateJob extends DefaultBatchConfiguration {
+
+  private final ChartService chartService;
+
+  @Bean
+  public Job updateChartJob(JobRepository jobRepository, Step updateChartStep) {
+    return new JobBuilder("updateChartJob", jobRepository)
+        .start(updateChartStep)
+        .build();
+  }
+
+  @Bean
+  public Step updateChartStep(JobRepository jobRepository) {
+    return new StepBuilder("updateChartStep", jobRepository)
+        .tasklet((contribution, chunkContext) -> {
+          log.info("updateChartStep 실행");
+          chartService.updateChart();
+
+          return RepeatStatus.FINISHED;
+        }, getTransactionManager())
+        .build();
+  }
+
+}
