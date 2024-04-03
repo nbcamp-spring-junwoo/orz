7187c2c5066ae4107e2e1d77881ce4775e6ac393
gloz0315 <qudtn0315@naver.com>
2024-04-03 20:33:01 +0900

    🐛 fix(coupon): 배치 읽을 때 버그 수정

--- src/main/java/com/junwoo/ott/global/batch/reader/CouponIssuanceReader.java
+++ src/main/java/com/junwoo/ott/global/batch/reader/CouponIssuanceReader.java
@@ -3,11 +3,9 @@ package com.junwoo.ott.global.batch.reader;
 import com.junwoo.ott.domain.coupon.entity.CouponIssuance;
 import com.junwoo.ott.domain.coupon.repository.CouponIssuanceRepository;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.item.ItemReader;
-import org.springframework.batch.item.NonTransientResourceException;
-import org.springframework.batch.item.ParseException;
-import org.springframework.batch.item.UnexpectedInputException;
 import org.springframework.stereotype.Component;
 
 @RequiredArgsConstructor
@@ -17,14 +15,20 @@ public class CouponIssuanceReader implements ItemReader<CouponIssuance> {
   private final CouponIssuanceRepository couponIssuanceRepository;
 
   private int currentIndex = 0;
-  private final LocalDate localDate = LocalDate.now();
+  private LocalDateTime localDate = LocalDateTime.now();
 
   @Override
   public CouponIssuance read()
       throws Exception {
+    if (localDate.isBefore(LocalDateTime.now().minusMinutes(2))) {
+      currentIndex = 0;
+      localDate = LocalDateTime.now();
+    }
 
-    return (currentIndex < couponIssuanceRepository.getCouponIssuance(localDate).size())
-        ? couponIssuanceRepository.getCouponIssuance(localDate).get(currentIndex++) : null;
+    return (currentIndex < couponIssuanceRepository.getCouponIssuance(LocalDate.from(localDate))
+        .size())
+        ? couponIssuanceRepository.getCouponIssuance(LocalDate.from(localDate)).get(currentIndex++)
+        : null;
   }
 
 }
