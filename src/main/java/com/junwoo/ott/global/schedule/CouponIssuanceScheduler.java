16d0ed123206eb221ff905528c8534c70ae34334
gloz0315 <qudtn0315@naver.com>
2024-04-03 20:13:00 +0900

    ✨ feat(coupon): spirng batch 설정 및 스케줄러 등록

---
+++ src/main/java/com/junwoo/ott/global/schedule/CouponIssuanceScheduler.java
@@ -0,0 +1,30 @@
+package com.junwoo.ott.global.schedule;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.context.ApplicationContext;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class CouponIssuanceScheduler {
+
+  private final JobLauncher jobLauncher;
+  private final ApplicationContext context;
+
+  @Scheduled(cron = "0 0 0 * * ?")
+  public void runBatchJob() throws Exception {
+    Job job = context.getBean("couponIssuanceJob", Job.class);
+
+    JobParameters jobParameters = new JobParametersBuilder()
+        .addString("couponIssuanceJob", "times " + System.currentTimeMillis())
+        .toJobParameters();
+
+    jobLauncher.run(job, jobParameters);
+  }
+
+}
