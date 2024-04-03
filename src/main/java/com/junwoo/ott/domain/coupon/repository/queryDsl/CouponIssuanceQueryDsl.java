c34113ce446cbe09606436c2745dee96c91b17dd
gloz0315 <qudtn0315@naver.com>
2024-04-03 20:13:36 +0900

    ✨ feat(coupon): 현재 시각 기준으로 유효기간이 지난 쿠폰을 찾는 메서드 추가

--- src/main/java/com/junwoo/ott/domain/coupon/repository/queryDsl/CouponIssuanceQueryDsl.java
+++ src/main/java/com/junwoo/ott/domain/coupon/repository/queryDsl/CouponIssuanceQueryDsl.java
@@ -1,11 +1,15 @@
 package com.junwoo.ott.domain.coupon.repository.queryDsl;
 
 import com.junwoo.ott.domain.coupon.entity.CouponIssuance;
+import java.time.LocalDate;
+import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface CouponIssuanceQueryDsl {
 
   Page<CouponIssuance> getCouponIssuance(final Long userId, final Pageable pageable);
+
+  List<CouponIssuance> getCouponIssuance(final LocalDate localDate);
 
 }
--- src/main/java/com/junwoo/ott/domain/coupon/repository/queryDsl/CouponIssuanceQueryDslImpl.java
+++ src/main/java/com/junwoo/ott/domain/coupon/repository/queryDsl/CouponIssuanceQueryDslImpl.java
@@ -6,6 +6,7 @@ import com.junwoo.ott.domain.coupon.entity.CouponIssuance;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.time.LocalDate;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -35,12 +36,25 @@ public class CouponIssuanceQueryDslImpl implements CouponIssuanceQueryDsl {
     return PageableExecutionUtils.getPage(result, pageable, count::fetchOne);
   }
 
+  @Override
+  public List<CouponIssuance> getCouponIssuance(final LocalDate localDate) {
+
+    return jpaQueryFactory.select(couponIssuance)
+        .from(couponIssuance)
+        .where(couponEndAtExpired(localDate))
+        .fetch();
+  }
+
   private BooleanExpression userIdEq(final Long userId) {
     return userId != null ? couponIssuance.user.userId.eq(userId) : null;
   }
 
   private BooleanExpression notUsedAndUserIdEq(final Long userId) {
     return couponIssuance.usedAt.isNull().and(userIdEq(userId));
+  }
+
+  private BooleanExpression couponEndAtExpired(final LocalDate localDate) {
+    return couponIssuance.usedAt.isNull().and(couponIssuance.coupon.endAt.lt(localDate));
   }
 
 }
