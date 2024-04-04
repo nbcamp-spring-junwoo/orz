9769e6b7169bf74c7657d01aec3243d6f2556ff8
ruh0n <wns8256@naver.com>
2024-04-04 19:50:53 +0900

    ✅ test(card): 카드 서비스 테스트 코드 추가

--- src/test/java/com/junwoo/ott/domain/payment/service/CardServiceTest.java
+++ src/test/java/com/junwoo/ott/domain/payment/service/CardServiceTest.java
@@ -1,12 +1,22 @@
 package com.junwoo.ott.domain.payment.service;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
 
 import com.junwoo.ott.domain.payment.dto.request.CardCreateRequestDto;
+import com.junwoo.ott.domain.payment.dto.request.CardsReadRequestDto;
+import com.junwoo.ott.domain.payment.dto.response.CardReadRequestDto;
+import com.junwoo.ott.domain.payment.dto.response.CardResponseDto;
+import com.junwoo.ott.domain.payment.entity.Card;
 import com.junwoo.ott.domain.payment.repository.CardRepository;
+import com.junwoo.ott.domain.user.UserTestValues;
 import com.navercorp.fixturemonkey.FixtureMonkey;
-import com.navercorp.fixturemonkey.api.introspector.ConstructorPropertiesArbitraryIntrospector;
+import com.navercorp.fixturemonkey.api.introspector.BuilderArbitraryIntrospector;
+import java.util.List;
+import java.util.Optional;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -15,19 +25,19 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-@ExtendWith({MockitoExtension.class})
-class CardServiceTest {
+@ExtendWith(MockitoExtension.class)
+class CardServiceTest implements UserTestValues {
 
   @Mock
   private CardRepository cardRepository;
   @InjectMocks
   private CardService cardService;
   private final FixtureMonkey fm = FixtureMonkey.builder()
-      .objectIntrospector(ConstructorPropertiesArbitraryIntrospector.INSTANCE)
+      .objectIntrospector(BuilderArbitraryIntrospector.INSTANCE)
       .build();
 
+  @DisplayName("createCard")
   @Nested
-  @DisplayName("카드 생성")
   class createCard {
 
     @DisplayName("성공")
@@ -41,6 +51,51 @@ class CardServiceTest {
 
       // then
       then(cardRepository).should().save(any());
+    }
+
+  }
+
+  @DisplayName("getCard")
+  @Nested
+  class getCard {
+
+    @DisplayName("성공")
+    @Test
+    void success() {
+      // given
+      CardReadRequestDto requestDto = fm.giveMeOne(CardReadRequestDto.class);
+      Card card = fm.giveMeBuilder(Card.class).set("user", TEST_USER).sample();
+
+      given(cardRepository.findById(any())).willReturn(Optional.of(card));
+
+      // when
+      CardResponseDto result = cardService.getCard(requestDto);
+
+      // then
+      assertNotNull(result);
+      then(cardRepository).should().findById(any());
+    }
+
+  }
+
+
+  @DisplayName("getCards")
+  @Nested
+  class getCards {
+
+    @DisplayName("성공")
+    @Test
+    void success() {
+      // given
+      CardsReadRequestDto requestDto = fm.giveMeBuilder(CardsReadRequestDto.class).sample();
+      Card card = fm.giveMeBuilder(Card.class).set("user", TEST_USER).sample();
+
+      given(cardRepository.findAllByUser_UserId(any())).willReturn(List.of(card));
+
+      List<CardResponseDto> result = cardService.getCards(requestDto);
+
+      // then
+      assertFalse(result.isEmpty());
     }
 
   }
