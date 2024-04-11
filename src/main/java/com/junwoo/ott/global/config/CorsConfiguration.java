804bf3c68e73362a2c9b0d8fbe93ab4dd6149212
ruh0n <wns8256@naver.com>
2024-04-11 14:12:16 +0900

    🔐 chore: cors 문제 해결

---
+++ src/main/java/com/junwoo/ott/global/config/CorsConfiguration.java
@@ -0,0 +1,22 @@
+package com.junwoo.ott.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@EnableWebMvc
+@Configuration
+public class CorsConfiguration implements WebMvcConfigurer {
+
+  @Override
+  public void addCorsMappings(final CorsRegistry registry) {
+    registry.addMapping("/**")
+        .allowCredentials(true)
+        .allowedOriginPatterns("*")
+        .allowedHeaders("*")
+
+        .exposedHeaders("Authorization");
+  }
+
+}
