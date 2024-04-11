846bc00db5870533ac004f73b74cd697738ea6fd
ksj58884 <ksj58884@gmail.com>
2024-04-12 03:27:45 +0900

    :art: fix(Episode): 응답 dto 개선

--- src/main/java/com/junwoo/ott/domain/episode/dto/response/EpisodeUpdateResponseDto.java
+++ src/main/java/com/junwoo/ott/domain/episode/dto/response/EpisodeUpdateResponseDto.java
@@ -1,6 +1,8 @@
 package com.junwoo.ott.domain.episode.dto.response;
 
 import com.junwoo.ott.domain.episode.entity.Episode;
+import com.junwoo.ott.global.customenum.MembershipType;
+import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -8,6 +10,18 @@ import lombok.Getter;
 @AllArgsConstructor
 public class EpisodeUpdateResponseDto {
 
-    private Episode episode;
+    private final Long episodeId;
+    private final String title;
+    private final LocalDateTime releasedAt;
+    private final MembershipType membershipType;
+    private final String videoLink;
+
+    public EpisodeUpdateResponseDto(Episode episode) {
+        this.episodeId = episode.getEpisodeId();
+        this.title = episode.getTitle();
+        this.releasedAt = episode.getReleasedAt();
+        this.membershipType = episode.getMembershipType();
+        this.videoLink = episode.getVideoLink();
+    }
 
 }
