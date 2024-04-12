24190a107598e6a787759bb000542a602d57f4a3
tktcod <hs060100@gmail.com>
2024-04-13 03:46:00 +0900

    :adhesive_bandage: chore(deploy): img 파일 tag 수정

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,19 +1,15 @@
 #!/bin/sh
 
-# AWS Parameter Store에서 환경 변수 로드
-parameters=$(aws ssm get-parameters-by-path --path "/" --recursive --with-decryption --query "Parameters[*].[Name,Value]" --output text)
-for line in $parameters; do
-    # 키와 값을 분리
-    key=$(echo $line | awk '{print $1}')
-    value=$(echo $line | awk '{print $2}')
+parameter_store_prefix="/"
+export_statement=$(aws ssm get-parameters-by-path \
+    --path "${parameter_store_prefix}" \
+    --region ap-northeast-2 --recursive \
+    --with-decryption \
+    | jq -r '.Parameters[] | "export " + (.Name | gsub("^/";"") | gsub("/"; "_") | ascii_upcase) + "=\"" + .Value + "\""' \
+    )
 
-    # 슬래시를 언더스코어로 변경하고, 대문자로 변환
-    # 예: /mysql/url -> MYSQL_URL
-    env_key=$(echo $key | sed 's|/|_|g' | tr '[:lower:]' '[:upper:]')
-
-    # 환경변수로 설정
-    eval export $env_key='$value'
-done
+# Evaluate the statements to load them into the current shell session
+eval $export_statement
 
 # 기존 Docker CMD 실행
 exec "$@"
