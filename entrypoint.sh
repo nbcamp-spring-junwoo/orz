d2633cea6af3e1957774c7fda0f5a1eacc2206ed
tktcod <hs060100@gmail.com>
2024-04-12 20:53:50 +0900

    :adhesive_bandage: chore(deploy): img 파일 tag 수정

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 # AWS Parameter Store에서 환경 변수 로드
 parameters=$(aws ssm get-parameters-by-path --path "/" --recursive --with-decryption --query "Parameters[*].[Name,Value]" --output text)
@@ -9,7 +9,7 @@ for line in $parameters; do
 
     # 슬래시를 언더스코어로 변경하고, 대문자로 변환
     # 예: /mysql/url -> MYSQL_URL
-    env_key=$(echo $key | sed 's|/|-|g' | tr '-' '.' | tr '[:lower:]' '[:upper:]')
+    env_key=$(echo $key | sed 's|/|_|g' | tr '[:lower:]' '[:upper:]')
 
     # 환경변수로 설정
     eval export $env_key='$value'
