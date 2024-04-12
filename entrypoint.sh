5cdd8c0b40db3af08921d9e2750882f1b6279900
tktcod <hs060100@gmail.com>
2024-04-12 11:01:17 +0900

    :rocket: 빌드 파일에 환경변수 조회 스크립트 추가

---
+++ entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# AWS Parameter Store에서 환경 변수 로드
+parameters=$(aws ssm get-parameters-by-path --path "/" --recursive --with-decryption --query "Parameters[*].[Name,Value]" --output text)
+for line in $parameters; do
+    # 키와 값을 분리
+    key=$(echo $line | awk '{print $1}')
+    value=$(echo $line | awk '{print $2}')
+
+    # 슬래시를 언더스코어로 변경하고, 대문자로 변환
+    # 예: /mysql/url -> MYSQL_URL
+    env_key=$(echo $key | sed 's|/|-|g' | tr '-' '.' | tr '[:lower:]' '[:upper:]')
+
+    # 환경변수로 설정
+    eval export $env_key='$value'
+done
+
+# 기존 Docker CMD 실행
+exec "$@"
