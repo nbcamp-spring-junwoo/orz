059ef744c656bb13f2f84e497a4aa1c90f2363ac
tktcod <hs060100@gmail.com>
2024-04-13 17:19:13 +0900

    :sparkles: Load Balancer health check api 생성

---
+++ src/main/java/com/junwoo/ott/global/controller/HealthCheckController.java
@@ -0,0 +1,14 @@
+package com.junwoo.ott.global.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+  @GetMapping("/health")
+  public String checkHealth() {
+    return "OK";
+  }
+
+}
