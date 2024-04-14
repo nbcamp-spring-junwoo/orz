cdd777c44e5dc0bf36a481d547e60fa75e6c4099
ruh0n <wns8256@naver.com>
2024-04-14 18:02:26 +0900

    💬 fix(genre): update genre type

--- src/main/java/com/junwoo/ott/global/customenum/GenreType.java
+++ src/main/java/com/junwoo/ott/global/customenum/GenreType.java
@@ -4,25 +4,26 @@ import lombok.Getter;
 
 @Getter
 public enum GenreType {
-  COMEDY("COMEDY"),
-  ACTION("ACTION"),
-  ROMANCE("ROMANCE"),
-  FANTASY("FANTASY"),
-  ADVENTURE("ADVENTURE"),
-  THRILLER("THRILLER"),
-  SPORTS("SPORTS"),
-  WAR("WAR"),
-  HORROR("HORROR"),
-  DETECTIVE("DETECTIVE"),
-  CYBERPUNK("CYBERPUNK"),
-  METAFICTION("METAFICTION"),
+
+  ACTION("액션"),
+  ADVENTURE("모험"),
+  ANIMATION("애니메이션"),
+  COMEDY("코미디"),
+  CRIME("범죄"),
+  DOCUMENTARY("다큐멘터리"),
+  DRAMA("드라마"),
+  FAMILY("가족"),
+  FANTASY("판타지"),
+  HISTORY("역사"),
+  HORROR("공포"),
+  MUSIC("음악"),
   MYSTERY("MYSTERY"),
-  MILITARY("MILITARY"),
-  BLACK_COMEDY("BLACK_COMEDY"),
-  SPACE_OPERA("SPACE_OPERA"),
-  WESTERN("WESTERN"),
-  SCIENCE_FICTION("SCIENCE_FICTION"),
-  DOCUMENTARY("DOCUMENTARY")
+  ROMANCE("로맨스"),
+  SCIENCE_FICTION("SF"),
+  THRILLER("스릴러"),
+  TV_MOVIE("TV 영화"),
+  WAR("전쟁"),
+  WESTERN("서부"),
   ;
 
   private final String genre;
