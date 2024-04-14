036cee73aa09071a57cb1d013ce2bd05f75dedf2
devJWL <jwlee4450@gmail.com>
2024-04-14 14:49:23 +0900

    ✨ feat(episode): 에피소드 url필드 추가

--- src/main/java/com/junwoo/ott/domain/episode/dto/response/EpisodeProjectionDto.java
+++ src/main/java/com/junwoo/ott/domain/episode/dto/response/EpisodeProjectionDto.java
@@ -10,16 +10,18 @@ public class EpisodeProjectionDto {
   private final Long episodeId;
   private final String title;
   private final String description;
+  private final String episodeUrl;
   private final LocalDate releasedAt;
 
   @QueryProjection
   public EpisodeProjectionDto(
       final Long episodeId, final String title,
-      final String description, final LocalDate releasedAt
+      final String description, final String episodeUrl, final LocalDate releasedAt
   ) {
     this.episodeId = episodeId;
     this.title = title;
     this.description = description;
+    this.episodeUrl = episodeUrl;
     this.releasedAt = releasedAt;
   }
 
