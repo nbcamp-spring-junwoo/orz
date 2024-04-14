af4822723347a219280a3d2731d45e33674292ee
devJWL <jwlee4450@gmail.com>
2024-04-14 23:55:32 +0900

    🎨 feat(episode): 에피소드 테이블 episodeId와 title을 이용한 유일키 설정

--- src/main/java/com/junwoo/ott/domain/episode/entity/Episode.java
+++ src/main/java/com/junwoo/ott/domain/episode/entity/Episode.java
@@ -6,6 +6,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import lombok.AccessLevel;
@@ -22,15 +24,22 @@ import org.hibernate.annotations.SQLRestriction;
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 @SQLDelete(sql = "update episode set deleted_at = NOW() where episode_id = ?")
 @SQLRestriction(value = "deleted_at is NULL")
+@Table(uniqueConstraints = {
+    @UniqueConstraint(
+        name = "EPISODEID_TITLE_UNIQUE",
+        columnNames = {"episode_id", "title"}
+    )
+})
 @Entity
 public class Episode extends Timestamped {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "episode_id")
   private Long episodeId;
   @Column(nullable = false)
   private Long videoId;
-  @Column(nullable = false)
+  @Column(nullable = false, name = "title")
   private String title;
   @Column(nullable = false)
   private String description;
