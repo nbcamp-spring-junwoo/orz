8e5a81e08b953fbfff41637bf372ef569c238981
devJWL <jwlee4450@gmail.com>
2024-04-14 18:04:54 +0900

    🐛 fix(video): 한 비디오에 중복된 장르가 추가되는 버그 수정

--- src/main/java/com/junwoo/ott/domain/genre/service/VideoGenreService.java
+++ src/main/java/com/junwoo/ott/domain/genre/service/VideoGenreService.java
@@ -1,11 +1,13 @@
 package com.junwoo.ott.domain.genre.service;
 
+import com.junwoo.ott.domain.genre.dto.response.GenreResponseDto;
 import com.junwoo.ott.domain.genre.entity.VideoGenre;
 import com.junwoo.ott.domain.genre.repository.VideoGenreRepository;
 import com.junwoo.ott.global.customenum.GenreType;
 import jakarta.transaction.Transactional;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -19,8 +21,19 @@ public class VideoGenreService {
   private final GenreService genreService;
 
   public void createVideoGenre(final Long videoId, final Set<GenreType> genreTypeSet) {
-    List<VideoGenre> videoGenreList = genreService.getAllIn(genreTypeSet).stream()
-        .map(genreResponseDto -> new VideoGenre(videoId, genreResponseDto.getGenreId())).toList();
+    Set<Long> genreIdList = genreService.getAllIn(genreTypeSet).stream()
+        .map(GenreResponseDto::getGenreId)
+        .collect(Collectors.toSet());
+
+    Set<Long> alreadyGenreIdSet = videoGenreRepository.findAllByVideoId(videoId).stream()
+        .map(VideoGenre::getGenreId)
+        .collect(Collectors.toSet());
+
+    genreIdList.removeAll(alreadyGenreIdSet);
+
+    List<VideoGenre> videoGenreList = genreIdList.stream().map(
+        genreId -> new VideoGenre(videoId, genreId)
+    ).toList();
 
     videoGenreRepository.saveAll(videoGenreList);
   }
