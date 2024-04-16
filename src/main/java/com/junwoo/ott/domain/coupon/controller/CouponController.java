de6102f30865d3d78d59d9a5431a5dd5dff04a78
gloz0315 <qudtn0315@naver.com>
2024-04-16 21:26:55 +0900

    ✨ feat(coupon): 쿠폰 LockService 클래스 생성 및 동시성 문제 해결
    - 동시성 문제 해결
    - Coupon Admin 전용 컨트롤러 생성

--- src/main/java/com/junwoo/ott/domain/coupon/controller/CouponController.java
+++ src/main/java/com/junwoo/ott/domain/coupon/controller/CouponController.java
@@ -1,13 +1,9 @@
 package com.junwoo.ott.domain.coupon.controller;
 
-import com.junwoo.ott.domain.coupon.dto.body.CouponCreateDto;
-import com.junwoo.ott.domain.coupon.dto.body.CouponUpdateDto;
-import com.junwoo.ott.domain.coupon.dto.request.CouponCreateRequestDto;
 import com.junwoo.ott.domain.coupon.dto.request.CouponIssuanceCreateRequestDto;
 import com.junwoo.ott.domain.coupon.dto.request.CouponReadRequestDto;
-import com.junwoo.ott.domain.coupon.dto.request.CouponUpdateRequestDto;
 import com.junwoo.ott.domain.coupon.dto.response.CouponIssuanceReadResponseDto;
-import com.junwoo.ott.domain.coupon.dto.response.CouponReadResponseDto;
+import com.junwoo.ott.domain.coupon.service.CouponLockService;
 import com.junwoo.ott.domain.coupon.service.CouponService;
 import com.junwoo.ott.global.common.dto.ResponseDto;
 import com.junwoo.ott.global.jwt.UserDetailsImpl;
@@ -18,15 +14,10 @@ import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.context.ApplicationContext;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -36,19 +27,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class CouponController {
 
   private final CouponService couponService;
+  private final CouponLockService couponLockService;
+
   private final JobLauncher jobLauncher;
   private final ApplicationContext applicationContext;
-
-  @Secured(value = "ROLE_ADMIN")
-  @GetMapping("/coupons")
-  public ResponseDto<Page<CouponReadResponseDto>> getCoupons(
-      final Pageable pageable
-  ) {
-    CouponReadRequestDto dto = new CouponReadRequestDto(pageable);
-    Page<CouponReadResponseDto> result = couponService.getCoupon(dto);
-
-    return ResponseDto.ok(result);
-  }
 
   @GetMapping("users/{userId}/coupons")
   public ResponseDto<Page<CouponIssuanceReadResponseDto>> getCoupons(
@@ -61,15 +43,6 @@ public class CouponController {
     return ResponseDto.ok(result);
   }
 
-  @Secured(value = "ROLE_ADMIN")
-  @PostMapping("/coupons")
-  public void postCoupon(
-      final @Validated @RequestBody CouponCreateDto dto
-  ) {
-    CouponCreateRequestDto createRequestDto = new CouponCreateRequestDto(dto);
-    couponService.createCoupon(createRequestDto);
-  }
-
   @PostMapping("/coupons/get-coupon/{couponId}")
   public void postCouponIssuance(
       final @PathVariable("couponId") Long couponId,
@@ -77,25 +50,7 @@ public class CouponController {
   ) {
     CouponIssuanceCreateRequestDto createRequestDto = new CouponIssuanceCreateRequestDto(
         couponId, userDetails.getUserId());
-    couponService.createCouponIssuance(createRequestDto);
-  }
-
-  @Secured(value = "ROLE_ADMIN")
-  @PutMapping("/coupons/{couponId}")
-  public void putCoupon(
-      final @PathVariable("couponId") Long couponId,
-      final @RequestBody CouponUpdateDto dto
-  ) {
-    CouponUpdateRequestDto couponUpdateRequestDto = new CouponUpdateRequestDto(couponId, dto);
-    couponService.updateCoupon(couponUpdateRequestDto);
-  }
-
-  @Secured(value = "ROLE_ADMIN")
-  @DeleteMapping("/coupons/{couponId}")
-  public void deleteCoupon(
-      @PathVariable("couponId") final Long couponId
-  ) {
-    couponService.deleteCoupon(couponId);
+    couponLockService.createCouponIssuanceLock(createRequestDto);
   }
 
   @GetMapping("/launch")
