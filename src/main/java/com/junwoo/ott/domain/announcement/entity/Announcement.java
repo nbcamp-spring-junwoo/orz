ab52eb040823f0be7624597ffb1d683a716ec798
gloz0315 <qudtn0315@naver.com>
2024-04-17 17:58:32 +0900

    ✨ feat(announcement): 엔티티 수정

--- src/main/java/com/junwoo/ott/domain/announcement/entity/Announcement.java
+++ src/main/java/com/junwoo/ott/domain/announcement/entity/Announcement.java
@@ -1,15 +1,14 @@
 package com.junwoo.ott.domain.announcement.entity;
 
-import com.junwoo.ott.domain.admin.entity.Admin;
+import com.junwoo.ott.domain.coupon.entity.Coupon;
 import com.junwoo.ott.global.common.entity.Timestamped;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
 import java.time.LocalDateTime;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -31,13 +30,19 @@ public class Announcement extends Timestamped {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long announcementId;
-  @ManyToOne(fetch = FetchType.LAZY)
-  @JoinColumn(name = "admin_id")
-  private Admin admin;
+  @OneToOne
+  @JoinColumn(name = "coupon_id")
+  private Coupon coupon;
   @Column(nullable = false)
   private String title;
   @Column(nullable = false)
   private String content;
   private LocalDateTime deletedAt;
+
+  public void updateAnnouncement(final Coupon coupon, final String title, final String content) {
+    this.coupon = (coupon == null) ? this.coupon : coupon;
+    this.title = (title == null) ? this.title : title;
+    this.content = (content == null) ? this.content : content;
+  }
 
 }
