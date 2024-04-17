9a6c6a289a64a79893b6a7a8912850986c3c856d
gloz0315 <qudtn0315@naver.com>
2024-04-17 23:07:19 +0900

    ✨ feat(announcement): 공지사항 전체 조회 기능 추가

---
+++ src/main/java/com/junwoo/ott/domain/announcement/dto/body/AnnouncementReadDto.java
@@ -0,0 +1,13 @@
+package com.junwoo.ott.domain.announcement.dto.body;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class AnnouncementReadDto {
+
+  private Long announcementId;
+  private String announcementTitle;
+
+}
