bd324df659537b650d33512f7fbff2105321a02d
gloz0315 <qudtn0315@naver.com>
2024-04-17 17:58:10 +0900

    ✨ feat(announcement): service 로직 구현
    - CRUD에 대한 기능

---
+++ src/main/java/com/junwoo/ott/domain/announcement/service/AnnouncementService.java
@@ -0,0 +1,78 @@
+package com.junwoo.ott.domain.announcement.service;
+
+import com.junwoo.ott.domain.announcement.dto.request.AnnouncementCreateRequestDto;
+import com.junwoo.ott.domain.announcement.dto.request.AnnouncementDeleteRequestDto;
+import com.junwoo.ott.domain.announcement.dto.request.AnnouncementReadRequestDto;
+import com.junwoo.ott.domain.announcement.dto.request.AnnouncementUpdateRequestDto;
+import com.junwoo.ott.domain.announcement.dto.response.AnnouncementCreateResponseDto;
+import com.junwoo.ott.domain.announcement.dto.response.AnnouncementReadResponseDto;
+import com.junwoo.ott.domain.announcement.dto.response.AnnouncementUpdateResponseDto;
+import com.junwoo.ott.domain.announcement.entity.Announcement;
+import com.junwoo.ott.domain.announcement.repository.AnnouncementRepository;
+import com.junwoo.ott.domain.coupon.entity.Coupon;
+import com.junwoo.ott.domain.coupon.service.CouponService;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Transactional
+@Service
+public class AnnouncementService {
+
+  private final CouponService couponService;
+
+  private final AnnouncementRepository announcementRepository;
+
+  @Transactional(readOnly = true)
+  public AnnouncementReadResponseDto getAnnouncement(AnnouncementReadRequestDto dto) {
+    Announcement announcement = existAnnouncement(dto.getAnnouncementId());
+
+    return new AnnouncementReadResponseDto(announcement);
+  }
+
+  public AnnouncementCreateResponseDto createAnnouncement(
+      final AnnouncementCreateRequestDto createRequestDto
+  ) {
+    Coupon coupon = couponService.existCouponById(createRequestDto.getCouponId());
+
+    Announcement announcement = Announcement.builder()
+        .title(createRequestDto.getTitle())
+        .content(createRequestDto.getContent())
+        .coupon(coupon)
+        .build();
+
+    Announcement savedAnnouncement = announcementRepository.save(announcement);
+    return new AnnouncementCreateResponseDto(savedAnnouncement);
+  }
+
+  public AnnouncementUpdateResponseDto updateAnnouncement(
+      final AnnouncementUpdateRequestDto updateRequestDto
+  ) {
+    Announcement announcement = existAnnouncement(updateRequestDto.getAnnouncementId());
+
+    if (updateRequestDto.getCouponId() != null) {
+      couponService.existCouponById(updateRequestDto.getCouponId());
+    }
+
+    Coupon coupon = couponService.existCouponById(updateRequestDto.getCouponId());
+
+    announcement.updateAnnouncement(coupon, updateRequestDto.getTitle(),
+        updateRequestDto.getContent());
+
+    return new AnnouncementUpdateResponseDto(announcement);
+  }
+
+  public void deleteAnnouncement(final AnnouncementDeleteRequestDto dto) {
+    Announcement announcement = existAnnouncement(dto.getAnnouncementId());
+
+    announcementRepository.delete(announcement);
+  }
+
+  private Announcement existAnnouncement(final Long announcementId) {
+    return announcementRepository.findById(announcementId)
+        .orElseThrow(() -> new EntityNotFoundException("해당 공지사항이 존재하지 않습니다."));
+  }
+
+}
