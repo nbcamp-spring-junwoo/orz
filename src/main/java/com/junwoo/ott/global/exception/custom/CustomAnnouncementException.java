294e2ff9ad02709f82553ed4a5a1829abbdbf9c1
gloz0315 <qudtn0315@naver.com>
2024-04-18 12:30:17 +0900

    ✨ feat(announcement): 공지사항에 대한 예외처리 기능 추가

---
+++ src/main/java/com/junwoo/ott/global/exception/custom/CustomAnnouncementException.java
@@ -0,0 +1,9 @@
+package com.junwoo.ott.global.exception.custom;
+
+public class CustomAnnouncementException extends RuntimeException {
+
+  public CustomAnnouncementException(final String message) {
+    super(message);
+  }
+
+}
