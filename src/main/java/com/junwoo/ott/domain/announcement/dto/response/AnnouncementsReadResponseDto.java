8ed12008b43b2724b7cd6869485b8c0b94cbe7af
gloz0315 <qudtn0315@naver.com>
2024-04-18 10:31:27 +0900

    🩹 fix(announcement): 조회 Dto 수정
    - 불필요한 클래스 삭제 및 dto 패키지 이동
    - 조회에 대한 Dto 내용 수정

--- src/main/java/com/junwoo/ott/domain/announcement/dto/response/AnnouncementsReadResponseDto.java
+++ src/main/java/com/junwoo/ott/domain/announcement/dto/response/AnnouncementsReadResponseDto.java
@@ -1,11 +1,11 @@
-package com.junwoo.ott.domain.announcement.dto.body;
+package com.junwoo.ott.domain.announcement.dto.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
-public class AnnouncementReadDto {
+public class AnnouncementsReadResponseDto {
 
   private Long announcementId;
   private String title;
