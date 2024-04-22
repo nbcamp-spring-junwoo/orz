51e0a1ee35bb1a024ced8196cebb2a354cd7ff8f
gloz0315 <qudtn0315@naver.com>
2024-04-22 20:00:56 +0900

    ✨  feat(elastic): VideoSearchResultDto 클래스 생성
    - 반환 데이터의 필드명이 카멜 케이스로 변경
    
    - 기존의 VideoSearchDto -> VideoSearchResultDto 클래스로 변경하여 안의 필드값에 카멜 케이스로 변경

--- src/main/java/com/junwoo/ott/global/search/dto/response/VideoSearchResponseDto.java
+++ src/main/java/com/junwoo/ott/global/search/dto/response/VideoSearchResponseDto.java
@@ -1,16 +1,21 @@
 package com.junwoo.ott.global.search.dto.response;
 
 import com.junwoo.ott.global.search.dto.body.VideoSearchDto;
+import com.junwoo.ott.global.search.dto.body.VideoSearchResultDto;
 import java.util.List;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
-@AllArgsConstructor
 public class VideoSearchResponseDto {
 
-  private List<VideoSearchDto> videoReadResponseDtoList;
-  private Long totalSize;
-  private Long totalPage;
+  private final List<VideoSearchResultDto> videos;
+  private final Long totalSize;
+  private final Long totalPage;
+
+  public VideoSearchResponseDto(List<VideoSearchDto> videos, Long totalPage, Long totalSize) {
+    this.videos = videos.stream().map(VideoSearchResultDto::new).toList();
+    this.totalSize = totalSize;
+    this.totalPage = totalPage;
+  }
 
 }
