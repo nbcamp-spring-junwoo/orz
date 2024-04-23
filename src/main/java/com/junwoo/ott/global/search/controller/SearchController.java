8ef0fe6a36b4f7121b6591e2dddc8b6367ca8fab
gloz0315 <qudtn0315@naver.com>
2024-04-23 15:17:38 +0900

    ♻️ refactor(elastic): 컨트롤러와 서비스의 반환 타입 및 dto 클래스 수정

--- src/main/java/com/junwoo/ott/global/search/controller/SearchController.java
+++ src/main/java/com/junwoo/ott/global/search/controller/SearchController.java
@@ -3,10 +3,11 @@ package com.junwoo.ott.global.search.controller;
 import com.junwoo.ott.global.common.dto.ResponseDto;
 import com.junwoo.ott.global.search.dto.request.VideoSearchRequestDto;
 import com.junwoo.ott.global.search.dto.response.SearchResponseDto;
-import com.junwoo.ott.global.search.dto.response.VideoRandomSearchResponseDto;
-import com.junwoo.ott.global.search.dto.response.VideoSearchResponseDto;
+import com.junwoo.ott.global.search.dto.response.VideoRandomResponseDto;
+import com.junwoo.ott.global.search.dto.response.VideoResponseDto;
 import com.junwoo.ott.global.search.service.SearchService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -23,22 +24,22 @@ public class SearchController {
   public ResponseDto<SearchResponseDto> search(
       final @RequestParam("input") String input
   ) {
-    SearchResponseDto responseDto = searchService.search(input);
+    SearchResponseDto responseDto = searchService.titleSearch(input);
     return ResponseDto.ok(responseDto);
   }
 
   @GetMapping("/videos/search")
-  public ResponseDto<VideoSearchResponseDto> searchVideos(
+  public ResponseDto<Page<VideoResponseDto>> searchVideos(
       final @RequestParam String input,
       final @RequestParam(required = false) Integer page
   ) {
     VideoSearchRequestDto dto = new VideoSearchRequestDto(input, page);
-    VideoSearchResponseDto responseDto = searchService.getVideosElasticSearch(dto);
+    Page<VideoResponseDto> responseDto = searchService.getVideosElasticSearch(dto);
     return ResponseDto.ok(responseDto);
   }
 
   @GetMapping("/videos/random")
-  public ResponseDto<VideoRandomSearchResponseDto> searchRandomVideos() {
+  public ResponseDto<VideoRandomResponseDto> searchRandomVideos() {
     return ResponseDto.ok(searchService.getRandomVideosElasticSearch());
   }
 
