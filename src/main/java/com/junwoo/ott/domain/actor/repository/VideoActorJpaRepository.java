70f3a13bbe8e3f5317b16a32f72de4269435892d
tktcod <hs060100@gmail.com>
2024-04-24 22:31:43 +0900

    :fire: 사용하지 않는 도메인 삭제
    Actor, Comment, Director, Favorite, Star 삭제

--- src/main/java/com/junwoo/ott/domain/actor/repository/VideoActorJpaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.junwoo.ott.domain.actor.repository;
-
-import com.junwoo.ott.domain.actor.entity.VideoActor;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VideoActorJpaRepository extends JpaRepository<VideoActor, Long> {
-
-}
--- src/main/java/com/junwoo/ott/domain/star/repository/StarRepository.java
+++
@@ -1,8 +0,0 @@
-package com.junwoo.ott.domain.star.repository;
-
-import com.junwoo.ott.domain.star.entity.Star;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StarRepository extends JpaRepository<Star, Long> {
-
-}
