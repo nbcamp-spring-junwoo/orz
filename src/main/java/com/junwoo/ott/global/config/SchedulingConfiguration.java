2a8475c0043f83c3b6a933c82ba4372bac3c4c73
tktcod <hs060100@gmail.com>
2024-04-25 11:29:02 +0900

    :arrow_up: chore(schedule): shedLock 의존성 및 설정 추가

--- src/main/java/com/junwoo/ott/global/config/SchedulingConfiguration.java
+++ src/main/java/com/junwoo/ott/global/config/SchedulingConfiguration.java
@@ -1,10 +1,31 @@
 package com.junwoo.ott.global.config;
 
+import javax.sql.DataSource;
+import lombok.RequiredArgsConstructor;
+import net.javacrumbs.shedlock.core.LockProvider;
+import net.javacrumbs.shedlock.provider.jdbctemplate.JdbcTemplateLockProvider;
+import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
+@EnableSchedulerLock(defaultLockAtMostFor = "10m")
+@RequiredArgsConstructor
 @Configuration
 public class SchedulingConfiguration {
+
+  private final DataSource dataSource;
+
+  @Bean
+  public LockProvider lockProvider() {
+    return new JdbcTemplateLockProvider(
+        JdbcTemplateLockProvider.Configuration.builder()
+            .withJdbcTemplate(new JdbcTemplate(dataSource))
+            .usingDbTime()
+            .build()
+    );
+  }
 
 }
