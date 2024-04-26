e0ac227f2954dc8d26f9ddacbb1a12acc6503991
gloz0315 <qudtn0315@naver.com>
2024-04-26 09:08:43 +0900

    🩹 fix(elastic): 제목 한글자 검색 기능 적용
    - SearchType 변경

--- src/main/java/com/junwoo/ott/global/customenum/SearchType.java
+++ src/main/java/com/junwoo/ott/global/customenum/SearchType.java
@@ -1,14 +1,6 @@
 package com.junwoo.ott.global.customenum;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
-@Getter
-@AllArgsConstructor
 public enum SearchType {
-  TITLE("title"),
-  DESCRIPTION("description");
-
-  private final String type;
-
+  TITLE,
+  DESCRIPTION
 }
