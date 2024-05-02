c6af563e952386ca5afef4edf782bba5db397a68
gloz0315 <qudtn0315@naver.com>
2024-05-02 10:41:08 +0900

    ♻️ refactor(like): Toggle 기능을 통한 생성 및 삭제 기능 통합
    - DTO 클래스 이름 변경

--- src/main/java/com/junwoo/ott/domain/like/controller/LikeController.java
+++ src/main/java/com/junwoo/ott/domain/like/controller/LikeController.java
@@ -1,8 +1,8 @@
 package com.junwoo.ott.domain.like.controller;
 
-import com.junwoo.ott.domain.like.dto.request.LikeCreateRequestDto;
 import com.junwoo.ott.domain.like.dto.request.LikeReadRequestDto;
 import com.junwoo.ott.domain.like.dto.request.LikeRequestDto;
+import com.junwoo.ott.domain.like.dto.request.LikeToggleRequestDto;
 import com.junwoo.ott.domain.like.dto.response.LikeReadResponseDto;
 import com.junwoo.ott.domain.like.service.LikeService;
 import com.junwoo.ott.global.common.dto.ResponseDto;
@@ -11,7 +11,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -30,38 +29,29 @@ public class LikeController {
       final @PathVariable("videoId") Long videoId,
       final @AuthenticationPrincipal UserDetailsImpl userDetails
   ) {
-    LikeCreateRequestDto request = new LikeCreateRequestDto(userDetails.getUserId(), videoId);
-    likeService.createLike(request);
+    LikeToggleRequestDto request = new LikeToggleRequestDto(userDetails.getUserId(), videoId);
+    likeService.toggleLike(request);
   }
 
   @GetMapping("/me/likes")
-  public ResponseDto<Page<LikeReadResponseDto>> getLikes(
+  public ResponseDto<Page<LikeReadResponseDto>> getLikedVideos(
       final @AuthenticationPrincipal UserDetailsImpl userDetails,
       final Pageable pageable
   ) {
     LikeReadRequestDto request = new LikeReadRequestDto(userDetails.getUserId(), pageable);
-    Page<LikeReadResponseDto> response = likeService.getLikes(request);
+    Page<LikeReadResponseDto> response = likeService.getLikedVideos(request);
 
     return ResponseDto.ok(response);
   }
 
   @GetMapping("/me/likes/{videoId}")
-  public boolean getLike(
+  public ResponseDto<Boolean> getLike(
       final @PathVariable("videoId") Long videoId,
       final @AuthenticationPrincipal UserDetailsImpl userDetails
   ) {
     LikeRequestDto request = new LikeRequestDto(userDetails.getUserId(), videoId);
 
-    return likeService.getLike(request);
-  }
-
-  @DeleteMapping("/me/likes/{videoId}")
-  public void deleteLike(
-      final @PathVariable("videoId") Long videoId,
-      final @AuthenticationPrincipal UserDetailsImpl userDetails
-  ) {
-    LikeRequestDto request = new LikeRequestDto(userDetails.getUserId(), videoId);
-    likeService.deleteLike(request);
+    return ResponseDto.ok(likeService.getLike(request));
   }
 
 }
