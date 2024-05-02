fc13ed11fa0ed69fed8f8a0d27c56fc4eb1c3bec
gloz0315 <qudtn0315@naver.com>
2024-05-02 10:41:08 +0900

    ♻️ refactor(like): Toggle 기능을 통한 생성 및 삭제 기능 통합
    - DTO 클래스 이름 변경

--- src/main/java/com/junwoo/ott/domain/like/service/LikeService.java
+++ src/main/java/com/junwoo/ott/domain/like/service/LikeService.java
@@ -1,9 +1,8 @@
 package com.junwoo.ott.domain.like.service;
 
-import com.junwoo.ott.domain.like.dto.request.LikeCreateRequestDto;
 import com.junwoo.ott.domain.like.dto.request.LikeReadRequestDto;
 import com.junwoo.ott.domain.like.dto.request.LikeRequestDto;
-import com.junwoo.ott.domain.like.dto.response.LikeCreateResponseDto;
+import com.junwoo.ott.domain.like.dto.request.LikeToggleRequestDto;
 import com.junwoo.ott.domain.like.dto.response.LikeReadResponseDto;
 import com.junwoo.ott.domain.like.entity.Like;
 import com.junwoo.ott.domain.like.repository.LikeRepository;
@@ -11,6 +10,7 @@ import com.junwoo.ott.domain.user.entity.User;
 import com.junwoo.ott.domain.video.entity.Video;
 import com.junwoo.ott.domain.video.service.VideoService;
 import jakarta.persistence.EntityNotFoundException;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
@@ -25,21 +25,27 @@ public class LikeService {
 
   private final VideoService videoService;
 
-  public LikeCreateResponseDto createLike(final LikeCreateRequestDto dto) {
+  public void toggleLike(final LikeToggleRequestDto dto) {
     if (!videoService.isExistVideo(dto.getVideoId())) {
       throw new EntityNotFoundException("해당 비디오가 존재하지 않습니다.");
     }
 
+    Optional<Like> like = likeRepository.findLikeByUser_UserIdAndVideo_VideoId(dto.getUserId(),
+        dto.getVideoId());
+
+    if (like.isPresent()) {
+      likeRepository.delete(like.get());
+      return;
+    }
+
     User user = User.builder().userId(dto.getUserId()).build();
     Video video = Video.builder().videoId(dto.getVideoId()).build();
-    Like like = Like.of(user, video);
-    Like savedLike = likeRepository.save(like);
-
-    return new LikeCreateResponseDto(savedLike);
+    Like createLike = Like.of(user, video);
+    likeRepository.save(createLike);
   }
 
   @Transactional(readOnly = true)
-  public boolean getLike(final LikeRequestDto dto) {
+  public Boolean getLike(final LikeRequestDto dto) {
     if (!videoService.isExistVideo(dto.getVideoId())) {
       throw new EntityNotFoundException("해당 비디오가 존재하지 않습니다.");
     }
@@ -49,22 +55,10 @@ public class LikeService {
   }
 
   @Transactional(readOnly = true)
-  public Page<LikeReadResponseDto> getLikes(final LikeReadRequestDto dto) {
+  public Page<LikeReadResponseDto> getLikedVideos(final LikeReadRequestDto dto) {
     Page<Like> likes = likeRepository.findLikeByUser_UserId(dto.getUserId(), dto.getPageable());
 
     return likes.map(LikeReadResponseDto::new);
-  }
-
-  public void deleteLike(final LikeRequestDto dto) {
-    if (!likeRepository.existsLikeByUser_UserIdAndVideo_VideoId(dto.getUserId(),
-        dto.getVideoId())) {
-      throw new EntityNotFoundException("해당 찜이 존재하지 않습니다.");
-    }
-
-    Like like = likeRepository.findLikeByUser_UserIdAndVideo_VideoId(dto.getUserId(),
-        dto.getVideoId()).orElseThrow(() -> new EntityNotFoundException("해당 찜이 존재하지 않습니다."));
-
-    likeRepository.delete(like);
   }
 
 }
